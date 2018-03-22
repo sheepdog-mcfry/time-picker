ee49c1a5a0e23ec15c9105793de2e19eb6610cbc
Waseem Akbar <waseem@telenordigital.com>
2018-03-22 15:07:50 +0100

    Add functions and event listeners for minute clock

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -4,15 +4,23 @@ document.addEventListener('DOMContentLoaded', function(event) {
 });
 
 function initClockListeners() {
-  var clock = document.querySelector('.clock');
-  clock.addEventListener('click', setTime, false);
+  var hour = document.querySelector('.clock');
+  hour.addEventListener('click', setHour, false);
+
+  var minute = document.querySelector('.clock');
+  hour.addEventListener('click', setMinute, false);
 }
 
 function ensureTwoDigits(num) {
   return ('0' + num).slice(-2);
 }
 
-function setTime(e) {
-    var clickedItem = e.target.id;
-    document.getElementById('hoursInput').value = ensureTwoDigits(clickedItem);
+function setHour(e) {
+  var clickedItem = e.target.id;
+  document.getElementById('hoursInput').value = ensureTwoDigits(clickedItem);
+}
+
+function setMinute(e) {
+  var clickedItem = e.target.id;
+  document.getElementById('minutesInput').value = ensureTwoDigits(clickedItem);
 }
