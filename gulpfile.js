501dcd3ef0994687c24a31f32e2fa086209cd1f0
Waseem Akbar <waseem@telenordigital.com>
2018-03-22 11:22:16 +0100

    Add tmp folder for processing scss

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,25 @@ var gulpIf = require('gulp-if');
 var cssnano = require('gulp-cssnano');
 var useref = require('gulp-useref');
 var sass = require('gulp-sass')
+var clean = require('gulp-clean')
 
 gulp.task('sass', function () {
-  return gulp.src('src/scss/*.scss')
+  return gulp.src('src/sass/*.scss')
     .pipe(sass.sync().on('error', sass.logError))
-    .pipe(gulp.dest('src/css'));
+    .pipe(gulp.dest('src/tmp/css'));
 });
 
 gulp.task('compress', function() {
-  return gulp.src('src/*.html')
+  return gulp.src(['src/*.html'])
     .pipe(useref())
     .pipe(gulpIf('*.js', uglify()))
     .pipe(gulpIf('*.css', cssnano()))
     .pipe(gulp.dest('dist'))
 });
 
-gulp.task('build', ['sass', 'compress']);
+gulp.task('deleteTemp', function () {
+    return gulp.src('src/tmp/')
+        .pipe(clean({force: true}))
+});
+
+gulp.task('build', ['sass', 'compress', 'deleteTemp']);
