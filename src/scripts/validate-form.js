a324debc23a65f187aa5b5c91fb534778bc5d1d6
Waseem Akbar <waseem@telenordigital.com>
2018-03-23 08:54:05 +0100

    Add javascript validation to forms

---
+++ src/scripts/validate-form.js
@@ -0,0 +1,17 @@
+function validateForm() {
+    var hoursInput, minutesInput, ampm, text;
+
+    hoursInput = document.getElementById('hoursInput').value;
+    minutesInput = document.getElementById('minutesInput').value;
+    ampm = document.querySelector('input[name="time-of-day"]:checked').value;
+
+    if (isNaN(hoursInput) || hoursInput < 0 || hoursInput > 12 ) {
+      text = "Hours not valid";
+      alert(text);
+      return false
+    } else if (isNaN(minutesInput) || minutesInput < 0 || minutesInput > 60 ) {
+      text = "Minutes not valid";
+      alert(text);
+      return false
+    }
+}
